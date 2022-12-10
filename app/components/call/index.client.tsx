3753952326fffdabf74f335c6a7c3bf5b9b1bf44
Stefan Wirth <nafets.wirth@gmail.com>
2022-12-10 18:17:26 +0100

    Record audio stream from agora

--- app/components/call/index.client.tsx
+++ app/components/call/index.client.tsx
@@ -9,7 +9,12 @@ import {
   createClient,
   createMicrophoneAndCameraTracks,
 } from "agora-rtc-react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
+import RecordRTC, { invokeSaveAsDialog, StereoAudioRecorder } from "recordrtc";
+
+const appId = "7d83f415c4014ef0ac6b54caad9a5160";
+const token =
+  "007eJxTYKgT1JN2MK2bd/78+8rn9cvf3Nh0u2zBjP1/ntgt0egPb+VTYDBPsTBOMzE0TTYxMDRJTTNITDZLMjVJTkxMsUw0NTQzqO2ZktwQyMjwt7OekZEBAkF8FobkxJwcBgYA+XAiDQ==";
 
 export default function Call({ channelName = "call" }) {
   const config = { mode: "rtc", codec: "vp8" };
@@ -22,14 +27,50 @@ export default function Call({ channelName = "call" }) {
 
   const [users, setUsers] = useState<IAgoraRTCRemoteUser[]>([]);
   const [start, setStart] = useState<boolean>(false);
+  const recorder = useRef<any>();
+
+  const startRecording = useCallback((stream?: MediaStream) => {
+    if (!stream) {
+      return;
+    }
+
+    recorder.current = new RecordRTC(stream, {
+      type: "audio",
+      mimeType: "audio/webm;codecs=pcm", // endpoint requires 16bit PCM audio
+      recorderType: StereoAudioRecorder,
+      timeSlice: 30000, // set 250 ms intervals of data that sends to AAI
+      desiredSampRate: 16000,
+      numberOfAudioChannels: 1, // real-time requires only one channel
+      bufferSize: 4096,
+      audioBitsPerSecond: 128000,
+      ondataavailable: (blob: Blob) => {
+        console.log(`Received blob ${blob.size}`);
+        // invokeSaveAsDialog(blob);
+
+        const reader = new FileReader();
+        reader.onload = () => {
+          const base64data = reader.result;
+
+          // audio data must be sent as a base64 encoded string
+          // if (socket) {
+          //   socket.send(
+          //     JSON.stringify({ audio_data: base64data.split("base64,")[1] })
+          //   );
+          // }
+        };
+        reader.readAsDataURL(blob);
+      },
+    });
+
+    recorder.current?.startRecording();
+    // take the stream and start recording it to a file
+  }, []);
 
   useEffect(() => {
     // function to initialise the SDK
     let init = async (name: string) => {
-      console.log("init", name);
       client.on("user-published", async (user, mediaType) => {
         await client.subscribe(user, mediaType);
-        console.log("subscribe success");
         if (mediaType === "video") {
           setUsers((prevUsers) => {
             return [...prevUsers, user];
@@ -37,6 +78,11 @@ export default function Call({ channelName = "call" }) {
         }
         if (mediaType === "audio") {
           user.audioTrack?.play();
+          if (user.audioTrack) {
+            startRecording(
+              new MediaStream([user.audioTrack.getMediaStreamTrack()])
+            );
+          }
         }
       });
 
@@ -53,29 +99,22 @@ export default function Call({ channelName = "call" }) {
       });
 
       client.on("user-left", (user) => {
-        console.log("leaving", user);
         setUsers((prevUsers) => {
           return prevUsers.filter((User) => User.uid !== user.uid);
         });
       });
 
-      await client.join(
-        "7d83f415c4014ef0ac6b54caad9a5160",
-        name,
-        "007eJxTYKgT1JN2MK2bd/78+8rn9cvf3Nh0u2zBjP1/ntgt0egPb+VTYDBPsTBOMzE0TTYxMDRJTTNITDZLMjVJTkxMsUw0NTQzqO2ZktwQyMjwt7OekZEBAkF8FobkxJwcBgYA+XAiDQ==",
-        null
-      );
+      await client.join(appId, name, token, null);
       if (tracks) {
         await client.publish([tracks[0], tracks[1]]);
+        setStart(true);
       }
-      setStart(true);
     };
 
     if (ready && tracks && !start) {
-      console.log("init ready");
       init("call");
     }
-  }, [client, start, ready, tracks]);
+  }, [client, start, ready, tracks, startRecording]);
 
   return start && tracks && <Videos users={users} tracks={tracks} />;
 }
@@ -89,7 +128,7 @@ const Videos = (props: {
   return (
     <div id="videos">
       <AgoraVideoPlayer
-        className="min-h-[400px] min-w-[400px]"
+        className="h-[400px] w-[400px]"
         videoTrack={tracks[1]}
       />
       {users.length > 0 &&
@@ -97,7 +136,7 @@ const Videos = (props: {
           if (user.videoTrack) {
             return (
               <AgoraVideoPlayer
-                className="min-h-[400px] min-w-[400px]"
+                className="h-[400px] w-[400px]"
                 videoTrack={user.videoTrack}
                 key={user.uid}
               />
