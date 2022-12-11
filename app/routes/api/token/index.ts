b7f6f6899c48a0c5458d0f60a258c8eefa1f9b7b
Stefan Wirth <nafets.wirth@gmail.com>
2022-12-11 16:18:40 +0100

    Omsen

--- app/routes/api/token/index.ts
+++ app/routes/api/token/index.ts
@@ -1,7 +1,6 @@
-import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
 
-export async function loader({ params }: LoaderArgs) {
+export async function loader() {
   const apiKey = await generateApiKey();
 
   return json(apiKey);
