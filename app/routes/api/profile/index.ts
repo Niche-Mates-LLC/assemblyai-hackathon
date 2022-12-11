6f1dbf426abb78e4af58223af49048200372367d
Stefan Wirth <nafets.wirth@gmail.com>
2022-12-11 21:00:50 +0100

    Remove mock

--- app/routes/api/profile/index.ts
+++ app/routes/api/profile/index.ts
@@ -22,26 +22,6 @@ export async function loader({ request }: LoaderArgs) {
 }
 
 const getClearbitInfo = async (email: string) => {
-  // remove this when you want to get the info for other emails etc.
-  return {
-    person: {
-      twitter: {
-        handle: "NafetsWirth",
-        id: 2411164066,
-        bio: "The Calm Founder 😌\n\nStressed VC-Startup CTO turned bootstrapper to bring the calm to his biz.\n\nTweeting about creating leverage with processes and automation.",
-        followers: 2302,
-        following: 1247,
-        statuses: 19045,
-        favorites: 17990,
-        location: "Free systems for your SaaS  ➡️",
-        site: "https://www.calmbusinessos.com/templates",
-        avatar:
-          "https://pbs.twimg.com/profile_images/1600689157650743296/ECll5w2-.png",
-      },
-      linkedin: { handle: "in/stefan-wirth-5aa03593" },
-    },
-  };
-
   return await clearbit.Enrichment.find({
     email,
     stream: true,
